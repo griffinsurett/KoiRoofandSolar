5038af3df4e4f6f80363edc9f5859ea5839dc402
griffinjoshs <leveloutusa@gmail.com>
2025-04-25 01:32:09 -0400

    optimization attempt

---
+++ src/components/Phone.jsx
@@ -0,0 +1,14 @@
+// src/components/icons/PhoneIcon.jsx
+export default function PhoneIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-auto w-5"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        <path d="M6.62 10.79a15.053 15.053 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.09-.21 11.72 11.72 0 0 0 3.66 1.17 1 1 0 0 1 1 1V20a1 1 0 0 1-1 1A16 16 0 0 1 3 5a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1 11.72 11.72 0 0 0 1.17 3.66 1 1 0 0 1-.21 1.09l-2.2 2.2z" />
+      </svg>
+    );
+  }
+  
