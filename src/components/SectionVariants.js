d4c152c4448f9e8fdfa4b0875548dedf3219202c
griffinjoshs <leveloutusa@gmail.com>
2025-04-25 20:38:25 -0400

    image on top

--- src/components/SectionVariants.js
+++ src/components/SectionVariants.js
@@ -8,7 +8,7 @@ export const SectionVariants = {
     contentClass:
       "content-section flex flex-col justify-center py-[var(--spacing-xl)] lg:py-[var(--spacing-2xl)] p-[var(--spacing-lg)] md:p-[var(--spacing-md)]",
     itemsClass:
-      "w-full flex items-center justify-evenly flex-col lg:flex-row gap-[var(--spacing-md)] lg:gap-[var(--spacing-sm)]",
+    "w-full grid grid-cols-1 md:grid-cols-2 gap-[var(--spacing-md)] lg:gap-[var(--spacing-xl)]",
     itemClass: "",
     headingAreaClass: "flex flex-col text-bg",
     descriptionClass: "text-sm lg:text-xl text-bg py-[var(--spacing-sm)]",
