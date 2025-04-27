32ceae3a2d8062f3afe737b6ec571d0fdfbd4065
griffinjoshs <leveloutusa@gmail.com>
2025-04-26 22:10:38 -0400

    new footer changes

--- src/content/SiteData.js
+++ src/content/SiteData.js
@@ -4,7 +4,7 @@ import Logo from "@/assets/logo.png"
 export const SiteData = {
     title: "Koi Roofing and Solar",
     tagline: "Koi Roofing & Solar is a full-service contractor proudly serving New Jersey, Pennsylvania and all five boroughs of New York City", 
-    description: "At Koi Roofing & Solar, our mission is simple: to deliver beautiful, high-performance roofs and solar installations that protect your property and empower you with sustainable energy. Let us help you invest in both the safety of your building and the health of our planet.",
+    description: "At Koi Roofing & Solar, our mission is simple: to deliver beautiful, high-performance roofs and solar installations that protect your property and empower you with sustainable energy.",
     logo: {
         src: Logo,
         alt: "Griffin's logo",
