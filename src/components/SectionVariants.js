7468231a5bb6871bf1aad6e16b2b06acbe09d01e
griffinjoshs <leveloutusa@gmail.com>
2025-04-28 11:00:42 -0400

    hero image fallback

--- src/components/SectionVariants.js
+++ src/components/SectionVariants.js
@@ -4,7 +4,7 @@ export const SectionVariants = {
   // Variants for different section types
   media1: {
     sectionClass:
-      "section-lg flex text-center flex-col justify-center items-center py-[var(--spacing-sec-top)]",
+      "section-lg flex text-center flex-col justify-center items-center sec-spacing-y",
     contentClass:
       "content-section flex flex-col justify-center px-[var(--spacing-lg)] md:px-[var(--spacing-md)]",
     itemsClass:
@@ -22,13 +22,13 @@ export const SectionVariants = {
   },
   imageCol: {
     sectionClass:
-      "section-xs flex justify-center items-center text-left py-[var(--spacing-sec-top)]",
+      "section-xs flex justify-center items-center text-left sec-spacing-y",
     contentClass:
       "content-section flex flex-col-reverse md:flex-row justify-between items-start gap-[var(--spacing-xl)] xl:gap-[var(--spacing-3xl)]",
     itemsClass:
       "flex flex-col text-bg items-start justify-start p-[var(--spacing-sm)] gap-[var(--spacing-xl)] xl:gap-[var(--spacing-3xl)] w-full lg:w-2/3 lg:sticky lg:top-0",
     itemClass: "",
-    headingAreaClass: "flex flex-col py-[var(--spacing-2xl)]",
+    headingAreaClass: "flex flex-col mb-[50px]",
     buttonsSectionClass: "hidden",
     childSlotClass: "w-auto lg:w-1/3 lg:sticky lg:top-0",
     buttonsPlacement: "content-section",
@@ -38,11 +38,11 @@ export const SectionVariants = {
   },
   imageCards: {
     sectionClass:
-      "flex text-center flex-col section-sm justify-center items-center py-[var(--spacing-sec-top)]",
+      "flex text-center flex-col section-sm justify-center items-center sec-spacing-y",
     contentClass:
       "content-section flex flex-col justify-center py-[var(--spacing-md)] lg:py-[var(--spacing-2xl)] px-[var(--spacing-lg)] md:px-[var(--spacing-md)]",
     itemsClass:
-      "w-full flex items-center justify-evenly flex-col lg:flex-row gap-[var(--spacing-md)] lg:gap-[var(--spacing-sm)] py-[var(--spacing-xl)]",
+      "w-full flex items-center justify-evenly flex-col lg:flex-row gap-[var(--spacing-md)] lg:gap-[var(--spacing-sm)] py-[var(--spacing-md)]",
     headingAreaClass:
       "flex flex-col gap-[var(--spacing-sm)] py-[var(--spacing-sm)] lg:py-[var(--spacing-md)]",
     buttonsSectionClass: "py-[var(--spacing-lg)] lg:py-[var(--spacing-xl)]",
